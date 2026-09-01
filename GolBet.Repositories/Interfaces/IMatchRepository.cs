801c84b64146209e17bb77b243661dfe9cc0a08a
Samuel Aristizabal <samuaristi17@gmail.com>
2026-08-31 19:29:50 -0500

    Add generic repository, match repository and database seeder (Module 3)

---
+++ GolBet.Repositories/Interfaces/IMatchRepository.cs
@@ -0,0 +1,18 @@
+﻿using GolBet.Entities;
+using GolBet.Entities.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GolBet.Repositories.Interfaces;
+public interface IMatchRepository : IGenericRepository<Match>
+
+{
+
+    Task<IEnumerable<Match>> GetAllWithTeamsAsync(MatchStatus? status = null);
+
+    Task<Match?> GetByIdWithDetailsAsync(int id);
+
+}
