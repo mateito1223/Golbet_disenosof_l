0276e8a0f44eaf6af3e142c3e015583e8f300f34
Samuel Aristizabal <samuaristi17@gmail.com>
2026-09-16 20:20:45 -0500

    Add services layer, DTOs, AutoMapper and match board (Module 4)

---
+++ GolBet.Services/Interfaces/IMatchService.cs
@@ -0,0 +1,19 @@
+﻿using GolBet.Entities.Enums;
+using GolBet.Services.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GolBet.Services.Interfaces;
+
+public interface IMatchService
+
+{
+
+    /// <summary>Match board: all active matches ordered by date.</summary> 
+
+    Task<IEnumerable<MatchDto>> GetBoardAsync(MatchStatus? status = null);
+
+}
---
+++ GolBet.Services/Mapping/MappingProfile.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using GolBet.Entities;
+using GolBet.Services.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GolBet.Services.Mapping;
+
+public class MappingProfile : Profile
+
+{
+
+    public MappingProfile()
+
+    {
+
+        // Flattening by convention: 
+
+        // MatchDto.HomeTeamName  <- Match.HomeTeam.Name 
+
+        // MatchDto.AwayTeamCrestUrl <- Match.AwayTeam.CrestUrl 
+
+        CreateMap<Match, MatchDto>();
+
+    }
+
+}
